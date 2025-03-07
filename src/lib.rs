b93af3a2d2b0332e1da3915478e61204510797aa
Matthieu Vachon <matt@streamingfast.io>
2025-03-07 12:05:26 -0500

    Updated to use `filtered_calls` instead of block directly

--- src/lib.rs
+++ src/lib.rs
@@ -2,49 +2,39 @@ mod abi;
 mod pb;
 use hex_literal::hex;
 use pb::contract::v1 as contract;
-use substreams::Hex;
-use substreams_ethereum::pb::eth::v2 as eth;
+use substreams::{pb::substreams::Clock, Hex};
 
 #[allow(unused_imports)]
 use num_traits::cast::ToPrimitive;
 use std::str::FromStr;
 use substreams::scalar::BigDecimal;
 
+use crate::pb::sf::substreams::ethereum::v1::Calls;
+
 substreams_ethereum::init!();
 
-const PRECOMPILE1_TRACKED_CONTRACT: [u8; 20] = hex!("0000000000000000000000000000000000000001");
+fn map_precompile1_calls(clock: &Clock, calls_message: &Calls, output: &mut contract::Calls) {
+    output
+        .precompile1_call_runs
+        .extend(&mut calls_message.calls.iter().filter_map(|call_info| {
+            let call = call_info.call.as_ref().unwrap();
 
-fn map_precompile1_calls(blk: &eth::Block, calls: &mut contract::Calls) {
-    calls.precompile1_call_runs.append(
-        &mut blk
-            .transactions()
-            .flat_map(|tx| {
-                tx.calls
-                    .iter()
-                    .filter(|call| {
-                        call.address == PRECOMPILE1_TRACKED_CONTRACT
-                            && abi::precompile1_contract::functions::Run::match_call(call)
-                    })
-                    .filter_map(|call| {
-                        match abi::precompile1_contract::functions::Run::decode(call) {
-                            Ok(decoded_call) => Some(contract::Precompile1RunCall {
-                                call_tx_hash: Hex(&tx.hash).to_string(),
-                                call_block_time: Some(blk.timestamp().to_owned()),
-                                call_block_number: blk.number,
-                                call_ordinal: call.begin_ordinal,
-                                call_success: !call.state_reverted,
-                            }),
-                            Err(_) => None,
-                        }
-                    })
-            })
-            .collect(),
-    );
+            match abi::precompile1_contract::functions::Run::decode(call) {
+                Ok(decoded_call) => Some(contract::Precompile1RunCall {
+                    call_tx_hash: call_info.tx_hash.clone(),
+                    call_block_time: clock.timestamp.clone(),
+                    call_block_number: clock.number,
+                    call_ordinal: call.begin_ordinal,
+                    call_success: !call.state_reverted,
+                }),
+                Err(_) => None,
+            }
+        }));
 }
 
 #[substreams::handlers::map]
-fn map_calls(blk: eth::Block) -> Result<contract::Calls, substreams::errors::Error> {
-    let mut calls = contract::Calls::default();
-    map_precompile1_calls(&blk, &mut calls);
-    Ok(calls)
+fn map_calls(clock: Clock, calls: Calls) -> Result<contract::Calls, substreams::errors::Error> {
+    let mut output = contract::Calls::default();
+    map_precompile1_calls(&clock, &calls, &mut output);
+    Ok(output)
 }
